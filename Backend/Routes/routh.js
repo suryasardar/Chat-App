1fcfbd3279f50071366917efb51e90731bded204
suryasardar <suryaprakashchary515@gmail.com>
2024-05-13 14:36:58 +0530

    started

---
+++ Backend/Routes/routh.js
@@ -0,0 +1,8 @@
+import express from "express";
+const router = express.Router();
+
+router.get("/login", (req, res) => {
+    res.send("login route");
+})
+
+export default router;
--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,24 @@
-cons
+import express from "express";
+import dotenv from "dotenv"
+import authRoutes from "./Routes/routh"
+dotenv.config();
+
+
+
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req,res) => {
+    res.send("Hello world !")
+})
+
+//Midddleware
+app.use("/api/auth",authRoutes);
+
+
+
+app.listen(PORT, () => {
+    console.log(`server listening on ${PORT}`);
+})
